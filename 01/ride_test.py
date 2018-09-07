88245db1d32137ca6be0edba6327ba7188710327
Matt Morgis (ELS-PHI) <m.morgis@elsevier.com>
2018-09-07 11:45:03 -0400

    test: mocking out file input!

--- 01/ride_test.py
+++ 01/ride_test.py
@@ -1,5 +1,7 @@
+from mock import (mock_open, patch)
 from ride import main
 
 
-def test_sample():
-    assert main() == ('COMETQ', 'HVNGAT')
+def test_open3():
+    with patch("ride.open", mock_open(read_data="Hello\nMock\n"), create=True):
+        assert ("Hello", "Mock") == main()
