72858f73b70f95b77fedafac4c5ebe2409005638
Matt Morgis (ELS-PHI) <m.morgis@elsevier.com>
2018-09-07 11:44:46 -0400

    feat: using `with` to open file b/c best practices

--- 01/ride.py
+++ 01/ride.py
@@ -6,10 +6,9 @@ TASK: ride
 
 
 def main():
-    input_file = open('ride.in', 'r')
-    output_file = open('ride.out', 'w')
-
-    group_name, comet_name = [line.strip() for line in input_file.readlines()]
+    with open('ride.in', 'r') as input_file:
+        group_name, comet_name = [line.strip()
+                                  for line in input_file.readlines()]
 
     return group_name, comet_name
 
