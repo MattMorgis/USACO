71c9a3ce522d5f8987cc6f1818e315f0998fd518
Matt Morgis (ELS-PHI) <m.morgis@elsevier.com>
2018-09-09 14:11:45 -0400

    test: first base case

--- 02/gift1_test.py
+++ 02/gift1_test.py
@@ -2,6 +2,6 @@ from mock import (mock_open, patch)
 from gift1 import main
 
 
-def test_sample():
-    with patch('gift1.open', mock_open(read_data='cometq\nHVNGAT\n'), create=True):
-        assert 'cometqHVNGAT' == main()
+def test_two_people_no_remainders():
+    with patch('gift1.open', mock_open(read_data='2\nmatt\ntara\nmatt\n100 1\ntara\ntara\n25 1\nmatt'), create=True):
+        assert 'matt 25\ntara 100' == main()
